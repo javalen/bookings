7c34287b487ae40a0950d7cb3ee4b48c3bdc06f2
Leonard Flournoy <lenflour@gmail.com>
2022-02-03 13:26:05 -0800

    Initial Commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"log"
+	"github.com/javalen/bookings/pkg/config"
+	"github.com/javalen/bookings/pkg/models"
+	"github.com/javalen/bookings/pkg/render"
+	"net/http"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+
+	stringMap["test"] = "Hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	log.Printf("THe remote IP is ", remoteIP)
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
